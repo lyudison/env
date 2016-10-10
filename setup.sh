004bcd51d7e311a0150585d1d4944fa544006666
Desheng Liu <lyudison@gmail.com>
2016-10-10 17:13:45 -0500

    Add C-family completion support flags file for YCM

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -s -i $ENV_DIR/zshrc ~/.zshrc
 ln -s -i $ENV_DIR/vimrc ~/.vimrc
 ln -s -i $ENV_DIR/tern-config ~/.tern-config
 ln -s -i $ENV_DIR/ctags ~/.ctags
+ln -s -i $ENV_DIR/ycm_extra_conf.py ~/.ycm_extra_conf.py
