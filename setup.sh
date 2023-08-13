aefa9af2da0b00fa33c5ff95344292fa87e4d3d3
lyudison@gmail.com <lyudison@gmail.com>
2023-08-13 14:50:05 -0700

    add tmux config and disable vim s no longer use

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-ENV_DIR=$(PWD)
+ENV_DIR=$PWD
 ln -s -i $ENV_DIR/zshrc ~/.zshrc
-ln -s -i $ENV_DIR/vimrc ~/.vimrc
+# TODO: Use VSCode instead of vim now
+# ln -s -i $ENV_DIR/vimrc ~/.vimrc
 ln -s -i $ENV_DIR/tern-config ~/.tern-config
 ln -s -i $ENV_DIR/ctags ~/.ctags
 ln -s -i $ENV_DIR/ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -s -i $ENV_DIR/tmux.conf ~/.tmux.conf
